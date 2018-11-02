9e4d5508d1c6da1c51982d68b285a5b5a8c2b5f1
thatguywiththatname <simjenner3@gmail.com>
2018-11-02 00:25:47 +0000

    Clear up comments

--- compileEXE.bat
+++ compileEXE.bat
@@ -3,7 +3,7 @@
 call compileGUI.bat
 cd StampCalculator
 :: Copy _rc.py files to the same location as the main Python file
-:: See app.pyw comment for why this happens
+:: See app.pyw comment(s) for why this happens
 copy gui\*_rc.py .
 pyinstaller --onefile --windowed --icon=gui/stamp.ico app.pyw
 :: Clean up
