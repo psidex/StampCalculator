03847a96b00ae993a8e508911d86f31b9c469022
thatguywiththatname <simjenner3@gmail.com>
2018-11-02 00:23:54 +0000

    Update colours, improve compileEXE, remove TODO(s)

--- compileEXE.bat
+++ compileEXE.bat
@@ -1,5 +1,14 @@
 :: Creates .exe
 @echo off
+call compileGUI.bat
 cd StampCalculator
+:: Copy _rc.py files to the same location as the main Python file
+:: See app.pyw comment for why this happens
+copy gui\*_rc.py .
 pyinstaller --onefile --windowed --icon=gui/stamp.ico app.pyw
+:: Clean up
+rd /s /q build
+del *.spec 
+del /Q *_rc.py
+cd ..
 echo Done
